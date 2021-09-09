e04b936b453f77df2ba11ec7513ed830c679ba50
Richard Deeming <RichardD2@users.noreply.github.com>
2021-09-09 16:51:03 +0100

    Aggressive inlining for .NET Standard 2.1; white-space style.

--- CSharp.Ulid/StringExtensions.cs
+++ CSharp.Ulid/StringExtensions.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Buffers;
+using System.Runtime.CompilerServices;
 
 namespace CSharp.Ulid
 {
     internal static class StringExtensions
     {
+#if NETSTANDARD2_1_OR_GREATER
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
         public static string Create<TState>(int length, TState state, SpanAction<char, TState> action)
         {
 #if NETSTANDARD2_0
@@ -12,17 +16,17 @@ namespace CSharp.Ulid
             if (action is null) throw new ArgumentNullException(nameof(action));
             if (length == 0) return string.Empty;
 
-			unsafe
-			{
-				var str = new string('\0', length);
-				fixed (char* chars = str)
-				{
-					var span = new Span<char>(chars, length);
-					action(span, state);
-				}
+            unsafe
+            {
+                var str = new string('\0', length);
+                fixed (char* chars = str)
+                {
+                    var span = new Span<char>(chars, length);
+                    action(span, state);
+                }
 
-				return str;
-			}
+                return str;
+            }
 #else
             return string.Create(length, state, action);
 #endif
